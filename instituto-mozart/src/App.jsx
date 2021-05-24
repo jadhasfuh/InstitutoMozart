fb01b0818ccfa731b24db9fac5b84021d18f7863
jadhasfuh <64806015+jadhasfuh@users.noreply.github.com>
2021-05-24 13:22:50 -0500

    Revision
    Revision

--- instituto-mozart/src/App.jsx
+++ instituto-mozart/src/App.jsx
@@ -3,6 +3,8 @@ import NavBar from './componentes/NavBar';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 import './assets/style/styles.css';
 import Home from './componentes/Home';
+import ReactDOM from 'react-dom';
+import MessengerCustomerChat from 'react-messenger-customer-chat';
 
 function App() {
 
@@ -14,6 +16,11 @@ function App() {
           <Route path='/' exact component={Home} />
         </Switch>
       </Router>
+      ReactDOM.render(
+      <MessengerCustomerChat
+        pageId="211411636121167"
+        appId="880827699137359"
+      />
     </>
   );
 }
